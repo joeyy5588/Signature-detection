b1bca2cf1cb658bef173c60ea72b8763d1ed66d3
ckpc21_mama <patrickwu2@gmail.com>
2019-08-05 11:07:32 +0800

    spectral norm

--- model/UNET.py
+++ model/UNET.py
@@ -9,11 +9,9 @@ class double_conv(nn.Module):
     def __init__(self, in_ch, out_ch):
         super(double_conv, self).__init__()
         self.conv = nn.Sequential(
-            nn.Conv2d(in_ch, out_ch, 3, padding=1),
-            nn.BatchNorm2d(out_ch),
+            nn.utils.spectral_norm(nn.Conv2d(in_ch, out_ch, 3, padding=1)),
             nn.ReLU(inplace=True),
-            nn.Conv2d(out_ch, out_ch, 3, padding=1),
-            nn.BatchNorm2d(out_ch),
+            nn.utils.spectral_norm(nn.Conv2d(out_ch, out_ch, 3, padding=1)),
             nn.ReLU(inplace=True)
         )
 
